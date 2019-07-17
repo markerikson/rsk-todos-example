3ff45f963f86bcd1c545e49e9c7dfbdfcacccd5c
Mark Erikson <mark@isquaredsoftware.com>
2019-07-17 00:12:45 -0400

    Add the filter reducer to the store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "redux-starter-kit";
 
 import todosReducer from "./todos";
+import filterReducer from "./visibilityFilters";
 
 export const store = configureStore({
   reducer: {
-    todos: todosReducer
+    todos: todosReducer,
+    visibilityFilter: filterReducer
   }
 });
